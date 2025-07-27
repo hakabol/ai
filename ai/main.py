e09c3fd497df27fc1cac06d261f2d8a86da23174
hakabol <habelcijuanu@gmail.com>
2025-07-27 19:25:49 +0530

    Update main.py

--- ai/main.py
+++ ai/main.py
@@ -1,1 +1,32 @@
+from model import chatbot_assistance
+import streamlit as st
+import os
 
+st.title("AI")
+
+assistant = chatbot_assistance()
+current_dir = os.path.dirname(__file__)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+settings_path = os.path.join(current_dir, "settings.py")
+
+assistant.load_settings(settings_path)
+
+assistant.prepare_data()
+model_path = os.path.join(current_dir, "chatbot_model.pth")
+dimensions_path = os.path.join(current_dir, "dimensions.json")
+assistant.load(model_path, dimensions_path)
+
+if 'message' not in st.session_state:
+    st.session_state.message = []
+
+message = st.chat_input()
+
+for mess in st.session_state.message:
+    st.chat_message(mess["role"]).markdown(mess["content"])
+
+if message:
+    st.chat_message("user").markdown(message)
+    st.session_state.message.append({"role":"user", "content":message})
+    output = assistant.process_message(message)
+    st.chat_message("ai").markdown(output)
+    st.session_state.message.append({"role":"ai", "content":output})
